b1aaf92e906b2d10c1db50d00d96339cc3404ebe
RocherKong <rocher@126.com>
2016-12-13 00:05:12 +0800

    servicecollection config

--- Develpment/BlogClub/src/BlogClub/Startup.cs
+++ Develpment/BlogClub/src/BlogClub/Startup.cs
@@ -31,6 +31,9 @@ namespace BlogClub
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+            services.AddOptions();
+          //  services.AddDbContext
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
