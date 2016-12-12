c93aeacb946f40fb4b46621901416cd3c94802dc
RocherKong <rocher@126.com>
2016-12-12 23:56:37 +0800

    StartUp Config

--- Develpment/BlogClub/src/BlogClub/Startup.cs
+++ Develpment/BlogClub/src/BlogClub/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
 
 namespace BlogClub
 {
@@ -14,6 +15,20 @@ namespace BlogClub
     {
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfigurationRoot Configuration { get; set; }
+
+        public Startup()
+        {
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddJsonFile("appsettings.json", optional: true);
+
+            ///生成配置
+            Configuration = builder.Build();
+
+
+        }
+
         public void ConfigureServices(IServiceCollection services)
         {
         }
