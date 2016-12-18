374788e928be6f0eab460df9b1c64b9d827f646b
Administrator <Administrator@APPLE>
2016-12-18 22:17:38 +0800

    增加简单的连接，插入、查询操作

--- Document/Demo/Core.Mongo/src/Core.Mongo.Console/Program.cs
+++ Document/Demo/Core.Mongo/src/Core.Mongo.Console/Program.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
 
 namespace Core.Mongo.Console
 {
@@ -9,7 +7,18 @@ namespace Core.Mongo.Console
     {
         public static void Main(string[] args)
         {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("foo");
+            var collection = database.GetCollection<BsonDocument>("bar");
 
+            collection.InsertOneAsync(new BsonDocument("Name", "Jack"));
+
+            var list = collection.Find(new BsonDocument("Name", "Jack")).ToList();
+
+            foreach (var document in list)
+            {
+                System.Console.WriteLine(document["Name"]);
+            }
         }
     }
 }
