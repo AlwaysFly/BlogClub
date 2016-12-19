c6c3616a7619030e18041b2b785ce41d7c2718e2
RocherKong <rocher@126.com>
2016-12-19 22:01:55 +0800

    add angular references to manage.web

---
+++ Develpment/BlogClub/BlogClub.Manage.Web/wwwroot/app/app.module.ts
@@ -0,0 +1,30 @@
+﻿import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { HttpModule } from "@angular/http";
+import { FormsModule } from "@angular/forms";
+
+import { AppRoutingModule } from "./app-routing.module";
+
+import { AuthService } from "./_services/auth.service";
+
+import { AppComponent } from "./app.component";
+import { HomeComponent } from "./home/home.component";
+import { LoginComponent } from "./login/login.component";
+
+@NgModule({
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        AppRoutingModule,
+        FormsModule
+    ],
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        LoginComponent
+    ],
+    providers: [AuthService]
+})
+export class AppModule { }
