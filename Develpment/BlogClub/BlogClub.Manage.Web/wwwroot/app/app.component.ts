63de8a1921f47d334814106be0893ea1ba70a443
RocherKong <rocher@126.com>
2016-12-19 19:05:00 +0800

    delete manage.web

--- Develpment/BlogClub/BlogClub.Manage.Web/wwwroot/app/app.component.ts
+++
@@ -1,9 +0,0 @@
-﻿import { Component } from '@angular/core';
-
-@Component({
-    moduleId: module.id,
-    selector: 'my-app',
-    template: "this is in angular2",
-})
-export class AppComponent {
-}
